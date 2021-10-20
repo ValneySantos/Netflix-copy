1b1440d6ce390c9f1943f0db97f02a373b403c5c
Valney <filhovalney@gmail.com>
2021-10-19 21:16:14 -0300

    js/

---
+++ js/setup.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        320:{
+            items:2.5
+        },
+        600:{
+            items:3.5
+        },
+        1000:{
+            items:4.5
+        }
+    }
+})
